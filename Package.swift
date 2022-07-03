fb9f59d6a7fcdfccd78c62df770cbe5d877365b9
Carson Katri <carson.katri@gmail.com>
2022-07-03 09:21:57 -0400

    Use remote Tokamak not local

--- Package.swift
+++ Package.swift
@@ -12,8 +12,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-//        .package(url: "https://github.com/TokamakUI/Tokamak", revision: "d36bfca"),
-        .package(path: "../Tokamak"),
+        .package(url: "https://github.com/TokamakUI/Tokamak", revision: "ca10bc2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
